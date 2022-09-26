3dfc0d49ab7fb46c95bedff650fb9331448aeaef
Matthew Roberts <mr232432@falmouth.ac.uk>
2022-09-26 13:38:59 +0100

    feat: Initial Player Controller push

---
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region VARIABLES
+    
+    #region REFERENCES
+    Rigidbody rb; // Ref to Player Rigidbody
+    CharacterController characterController; // Ref to char controller component
+
+    #endregion
+    #endregion
+}
