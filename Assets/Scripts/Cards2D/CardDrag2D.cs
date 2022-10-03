24b1684c9266309399d90e40c9acd49bef2542f0
Andrew Scott <as243879@falmouth.ac.uk>
2022-10-03 11:30:31 +0100

    feat: 2D Card Drag and Drop
    Card now highlights and scales slightly when hovered over

--- Assets/Scripts/Cards2D/CardDrag2D.cs
+++ Assets/Scripts/Cards2D/CardDrag2D.cs
@@ -2,27 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CardDrag2D : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class CardDrag2D : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     Vector2 offset;
 
+    public float hoverScale = 1.2f;
     public float pickupScale = 2;
-    Vector3 dropScale;
+    Vector3 baseScale;
+
+    public Image cardBackground;
+    Color baseColor;
+    public Color highlightColor;
 
     private void Start()
     {
-        dropScale = transform.localScale;
+        baseScale = transform.localScale;
+        baseColor = cardBackground.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter");
+        ScaleCard(hoverScale);
+        Highlight(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+        ScaleCard(1);
+        Highlight(false);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Drag Start");
 
+        Highlight(false);
+
         //Drags from where the player clicks instead of snapping center of card to the mouse
         offset = new Vector2(transform.position.x - eventData.position.x, transform.position.y - eventData.position.y);
 
-        transform.localScale = dropScale * pickupScale;
+        ScaleCard(pickupScale);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -35,6 +58,24 @@ public class CardDrag2D : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("Drag End");
-        transform.localScale = dropScale;
+        ScaleCard(hoverScale);
+        Highlight(true);
+    }
+
+    void Highlight(bool on)
+    {
+        if (on)
+        {
+            cardBackground.color = baseColor * highlightColor;
+        }
+        else
+        {
+            cardBackground.color = baseColor;
+        }
+    }
+
+    void ScaleCard(float scaleFactor)
+    {
+        transform.localScale = baseScale * scaleFactor;
     }
 }
