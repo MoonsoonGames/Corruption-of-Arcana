b3e0ce364813d519956c136a0edacc85dda61363
as243879 <as243879@falmouth.ac.uk>
2022-10-06 16:16:58 +0100

    feat: Dice Roller
    Text changes to the dice min and max values and also when dice are rolled

--- Assets/Scripts/Dice/DiceRoller.cs
+++ Assets/Scripts/Dice/DiceRoller.cs
@@ -7,9 +7,12 @@ public class DiceRoller : MonoBehaviour
 {
     Dice[] diceArray;
 
+    TextChanger textChanger;
+
     private void Start()
     {
         diceArray = GameObject.FindObjectsOfType<Dice>();
+        textChanger = GameObject.FindObjectOfType<TextChanger>();
     }
 
     public void RollDiceButton()
@@ -22,6 +25,18 @@ public class DiceRoller : MonoBehaviour
             totalValue += die.RollDice();
         }
 
-        Debug.Log(totalValue);
+        textChanger.AdjustTotal(totalValue);
+    }
+
+    public Vector2 GetDiceRange()
+    {
+        Vector2 totalRange = new Vector2(0, 0);
+
+        foreach (Dice die in diceArray)
+        {
+            totalRange += die.diceValues;
+        }
+
+        return totalRange;
     }
 }
