3b0e4572ad61265e311bac35affda403f1c2ee9a
Ryan Minty <RM248238@falmouth.ac.uk>
2022-10-31 17:32:12 +0000

    feat: Story Code
    Implementing story code

---
+++ Assets/Scripts/NPC/MouseClickControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MouseClickControl : MonoBehaviour
+{
+    Collider col;
+
+    private void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    public void EnableClicking()
+    {
+        col.enabled = true;
+    }
+    
+    public void DisableClicking()
+    {
+        col.enabled = false;
+    }
+}
