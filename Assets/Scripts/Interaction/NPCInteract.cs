be96075e5b4a84e1f06025037d1b3c16bbbf37bf
RYAN MINTY <RM248238@falmouth.ac.uk>
2022-11-04 11:50:07 +0000

    NPC Interraction
    Added in scripts to allow NPC's to be interacted with

---
+++ Assets/Scripts/Interaction/NPCInteract.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+/// <summary>
+/// Authored & Written by Andrew Scott andrewscott@icloud.com
+/// 
+/// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// </summary>
+namespace Necropanda
+{
+    public class NPCInteract : MonoBehaviour, IInteractable
+    {
+        public string SpeakToMessage = "SpeakingTo";
+
+            public void Interacted(GameObject player)
+            {
+                Flowchart.BroadcastFungusMessage(SpeakToMessage);
+                Debug.Log("Spoken to NPC");
+            }
+
+    }
+}
