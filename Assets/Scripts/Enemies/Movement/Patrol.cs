a1d16d7ab215ba233299e6f7dc828c041dfb1d1a
mr232432 <mr232432@falmouth.ac.uk>
2022-11-06 01:32:06 +0000

    refactor: Started rewriting patrol code
    Started rewriting patrol code. Moving away from unparenting gameobjects and looking to sample positions on the mesh.

--- Assets/Scripts/Enemies/Movement/Patrol.cs
+++ Assets/Scripts/Enemies/Movement/Patrol.cs
@@ -24,17 +24,22 @@ namespace Necropanda.AI.Movement
         public float timeToPatrol = 30f;
         public bool patrol = true;
         public GameObject CPatrolPoint;
+        
+        public Vector3 originalPos;
+        public Vector3[] patrolPoints;
 
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
-            CPatrolPoint = GameObject.FindGameObjectWithTag("CenterPatrolPoint");
-            pointsList.AddRange(GameObject.FindGameObjectsWithTag("PatrolPoint"));
-            foreach (GameObject point in pointsList)
-            {
-                point.transform.SetParent(null);
-            }
-            CPatrolPoint.transform.SetParent(null);
+
+            // OLD CODE
+            // CPatrolPoint = GameObject.FindGameObjectWithTag("CenterPatrolPoint");
+            // pointsList.AddRange(GameObject.FindGameObjectsWithTag("PatrolPoint"));
+            // foreach (GameObject point in pointsList)
+            // {
+            //     point.transform.SetParent(null);
+            // }
+            // CPatrolPoint.transform.SetParent(null);
 
             // Disabling auto-braking allows for continuous movement
             // between points (ie, the agent doesn't slow down as it
@@ -42,6 +47,33 @@ namespace Necropanda.AI.Movement
             agent.autoBraking = false;
             StartCoroutine(Cooldown(timeToPatrol));
             GotoNextPoint();
+        }
+
+        /// <summary>
+        /// Gets points in the 4 cardinal directions. Creates a diamond patrol pattern.
+        /// 
+        /// I hate how I've wrote this.. don't read it.
+        /// </summary>
+        /// <param name="offset">The offset amount to add to each direction.</param>
+        void GetNavHitPointsDiamond(float offset){
+            // Check to make sure offset isn't 0
+            if (offset == 0){
+                Debug.LogError("No offset added to patrol pattern, returning to avoid weird behaviour..");
+                return;
+            }
+
+            // Get the 4 points in the cardinal directions by adding the offset.
+            Vector3 north = new Vector3(transform.position.x + offset, transform.position.z);
+            Vector3 east = new Vector3(transform.position.x, transform.position.z - offset);
+            Vector3 south = new Vector3(transform.position.x - offset, transform.position.z);
+            Vector3 west = new Vector3(transform.position.x, transform.position.z + offset);
+
+
+            patrolPoints = new Vector3[4];
+            patrolPoints[0] = north;
+            patrolPoints[1] = east;
+            patrolPoints[2] = south;
+            patrolPoints[3] = west;
         }
 
 
@@ -61,19 +93,20 @@ namespace Necropanda.AI.Movement
 
         public void StopPatrol()
         {
-            agent.SetDestination(CPatrolPoint.transform.position);
-            agent.autoBraking = true;
-            if (agent.transform.position == CPatrolPoint.transform.position)
-            {
-                foreach (GameObject point in pointsList)
-                {
-                    point.transform.SetParent(this.gameObject.transform);
-                }
-                CPatrolPoint.transform.SetParent(this.gameObject.transform);
+            // OLD CODE
+            // agent.SetDestination(CPatrolPoint.transform.position);
+            // agent.autoBraking = true;
+            // if (agent.transform.position == CPatrolPoint.transform.position)
+            // {
+            //     foreach (GameObject point in pointsList)
+            //     {
+            //         point.transform.SetParent(this.gameObject.transform);
+            //     }
+            //     CPatrolPoint.transform.SetParent(this.gameObject.transform);
 
-                Debug.Log("Disabled the patrol movement module on " + this.name);
-                this.enabled = false;
-            }
+            //     Debug.Log("Disabled the patrol movement module on " + this.name);
+            //     this.enabled = false;
+            // }
         }
 
 
