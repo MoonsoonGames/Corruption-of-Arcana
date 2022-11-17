18f3d6a0e510730732319f9e13530a6fa83dbd40
Matthew Roberts <mr232432@falmouth.ac.uk>
2022-11-17 11:40:19 +0000

    fix: Cleaned up merge issue

--- Assets/Scripts/UI Handlers/HUDInterface.cs
+++ Assets/Scripts/UI Handlers/HUDInterface.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using NecroPanda.Player;
+using Necropanda.Player;
 
 public class HUDInterface : MonoBehaviour
 {
