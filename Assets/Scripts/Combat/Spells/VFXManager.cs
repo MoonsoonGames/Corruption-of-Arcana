e808f52743177fd9b54945bc078d6d15144f3f46
ANDREW SCOTT <as243879@falmouth.ac.uk>
2022-11-18 13:47:25 +0000

    feat: Combat
    -VFX direction is fixed VFX is not properly positioned

--- Assets/Scripts/Combat/Spells/VFXManager.cs
+++ Assets/Scripts/Combat/Spells/VFXManager.cs
@@ -62,7 +62,7 @@ namespace Necropanda
             targetPositions.Add(target.transform.position);
 
             VFXManager.instance.SpawnProjectile(caster.transform.position, targetPositions, spellRef.projectileObject, spellRef.trailColor, spellRef.impactObject, effectType);
-            SpawnCastEffect(caster.transform.position, target.transform.position, spellRef.castObject);
+            SpawnCastEffect(caster.gameObject.transform.position, target.gameObject.transform.position, spellRef.castObject);
             yield return new WaitForSeconds(effectDelay);
             spellRef.AffectTarget(caster, target, spell, effectType, cardsDiscarded, removedStatuses, empowered, weakened);
         }
@@ -110,8 +110,11 @@ namespace Necropanda
                 Debug.Log("No game object spawned, returning");
                 return;
             }
+
+            Debug.Log("Game object spawned, setting transform");
             effectObject.transform.position = spawnPosition;
-            effectObject.transform.rotation = Quaternion.FromToRotation(spawnPosition, targetPosition);
+            Vector2 direction = targetPosition - spawnPosition;
+            effectObject.transform.rotation = Quaternion.LookRotation(direction);
         }
 
         /// <summary>
