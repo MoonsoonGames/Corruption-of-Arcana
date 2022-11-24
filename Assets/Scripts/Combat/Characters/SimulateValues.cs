a361890c538ae20e14ef9dff3b14e3a8be0c9ead
Andrew Scott <as243879@falmouth.ac.uk>
2022-11-24 14:29:26 +0000

    chore: Comments
    -Added author comments to some scripts

--- Assets/Scripts/Combat/Characters/SimulateValues.cs
+++ Assets/Scripts/Combat/Characters/SimulateValues.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 
 /// <summary>
-/// Authored & Written by <NAME/TAG/SOCIAL LINK>
+/// Authored & Written by Andrew Scott andrewscott@icloud.com
 /// 
 /// Use by NPS is allowed as a collective, for external use, please contact me directly
 /// </summary>
