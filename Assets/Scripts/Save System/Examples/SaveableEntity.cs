aea5a0159399bfca111d6295c3b663ba26e22d16
mr232432 <mr232432@falmouth.ac.uk>
2022-11-25 14:30:36 +0000

    feat: Started work on save system
    lots more work and testing needed

---
+++ Assets/Scripts/Save System/Examples/SaveableEntity.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Written by @mattordev using Dino Dappers tutorial - https://www.youtube.com/watch?v=f5GvfZfy3yk&t=2s
+/// 
+/// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// </summary>
+namespace Necropanda.SaveSystem
+{
+    public class SaveableEntity : MonoBehaviour
+    {
+        [SerializeField] private string id = string.Empty;
+
+        public string Id => id;
+
+        [ContextMenu("Generate Id")]
+        private void GenerateId() => id = Guid.NewGuid().ToString();
+
+        public object CaptureState()
+        {
+            var state = new Dictionary<string, object>();
+
+            foreach (var saveable in GetComponents<ISaveable>())
+            {
+                state[saveable.GetType().ToString()] = saveable.CaptureState();
+            }
+
+            return state;
+        }
+    }
+}
---
+++ Assets/Scripts/Save System/Examples/SavingLoadingAdvancedExample.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Written by @mattordev using Dino Dappers tutorial - https://www.youtube.com/watch?v=f5GvfZfy3yk&t=2s
+/// 
+/// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// </summary>
+namespace Necropanda.SaveSystem
+{
+    public class SavingLoadingAdvancedExample : MonoBehaviour
+    {
+
+    }
+}
