41bb24a3bdd249ec33a5ad59a2f61d047b24fa95
mr232432 <mr232432@falmouth.ac.uk>
2022-11-28 13:18:57 +0000

    feat: First iteration of the save-system complete
    next task is to comment it all

--- Assets/Scripts/Save System/Examples/SaveableEntity.cs
+++ Assets/Scripts/Save System/Examples/SaveableEntity.cs
@@ -29,5 +29,20 @@ namespace Necropanda.SaveSystem
 
             return state;
         }
+
+        public void RestoreState(object state)
+        {
+            var stateDictionary = (Dictionary<string, object>)state;
+
+            foreach (var saveable in GetComponents<ISaveable>())
+            {
+                string typeName = saveable.GetType().ToString();
+
+                if (stateDictionary.TryGetValue(typeName, out object value))
+                {
+                    saveable.RestoreState(value);
+                }
+            }
+        }
     }
 }
