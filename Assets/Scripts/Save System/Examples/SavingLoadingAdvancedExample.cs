deba6d6e967d38108fb3edcd37b305ad9d8d0ca5
mr232432 <mr232432@falmouth.ac.uk>
2022-12-08 17:55:08 +0000

    feat/chore: Added Delete file function and updated comments
    Added function to delete the save file if there is one
    Added some more comments to reflect what I need to do still, aka a todo list.

--- Assets/Scripts/Save System/Examples/SavingLoadingAdvancedExample.cs
+++ Assets/Scripts/Save System/Examples/SavingLoadingAdvancedExample.cs
@@ -8,12 +8,22 @@ using UnityEngine;
 /// Written by @mattordev using Dino Dappers tutorial - https://www.youtube.com/watch?v=f5GvfZfy3yk&t=2s
 /// 
 /// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// 
+/// TODO:
+/// - Implement crash backup saving to prevent data corruption:
+///     before you write the save file, check if it exists, copy it, change extension to .bak
+///     save the new data and delete the .bak. 
+///     on load you can check for a .bak file, check if the date is newer, if so, rename current sav to .temp, 
+///         restore the .bak and recover the data
+///     delete the temp file.
+/// 
+/// - Refactor so a binary formatter isn't used.
 /// </summary>
 namespace Necropanda.SaveSystem
 {
     public class SavingLoadingAdvancedExample : MonoBehaviour
     {
-        private string SavePath => $"{Application.persistentDataPath}/save.txt";
+        private string SavePath => $"{Application.persistentDataPath}/save.dat";
 
         [ContextMenu("Save")]
         private void Save()
@@ -28,6 +38,15 @@ namespace Necropanda.SaveSystem
         {
             var state = LoadFile();
             RestoreState(state);
+        }
+
+        [ContextMenu("Delete Save Data")]
+        private void DeleteSaveData()
+        {
+            if (File.Exists(SavePath))
+            {
+                File.Delete(SavePath);
+            }
         }
 
         private Dictionary<string, object> LoadFile()
