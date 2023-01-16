4dfe81fc9f865d6dd4f890dedc3d0e0ba41e6837
AJScott <as243879@falmouth.ac.uk>
2023-01-16 11:28:07 +0000

    feat: Demo Day Prep
    -Updated nav mesh in thoth to ignore outside terrain
    -Added too to track quest state

---
+++ Assets/Scripts/NPC/MainQuestTracker.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+/// <summary>
+/// Authored & Written by <NAME/TAG/SOCIAL LINK>
+/// 
+/// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// </summary>
+namespace Necropanda
+{
+    public class MainQuestTracker : MonoBehaviour
+    {
+        public Flowchart flowChart;
+
+        public GameObject[] state1, state2, state3, state4, state5, state6;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            int questState = flowChart.GetIntegerVariable("QuestState");
+
+            Debug.Log("Quest state is " + questState);
+            
+            switch (questState)
+            {
+                case 1:
+                    foreach (var item in state1)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+                case 2:
+                    foreach (var item in state2)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+                case 3:
+                    foreach (var item in state3)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+                case 4:
+                    foreach (var item in state4)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+                case 5:
+                    foreach (var item in state5)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+                case 6:
+                    foreach (var item in state6)
+                    {
+                        item.SetActive(true);
+                    }
+                    break;
+            }
+        }
+    }
+}
