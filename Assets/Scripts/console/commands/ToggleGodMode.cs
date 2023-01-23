3c528db39888727f47d4e9c55f6d607f2eb0764b
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-01-23 14:37:00 +0000

    feat: started work on toggle god mode command

---
+++ Assets/Scripts/console/commands/ToggleGodMode.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Authored & Written by @mrobertscgd
+/// 
+/// Use by NPS is allowed as a collective, for external use, please contact me directly
+/// </summary>
+namespace Necropanda.Utils.Console.Commands
+{
+    [CreateAssetMenu(fileName = "New TGM Command", menuName = "Utilities/DeveloperConsole/Commands/TGM Command")]
+    /// <summary>
+    /// Allows the player to log text to the Unity Console.
+    /// </summary>
+    public class ToggleGodMode : ConsoleCommand
+    {
+        public override bool Process(string[] args)
+        {
+            // Get the text to log based on the args passed in.
+            string logText = string.Join(" ", args);
+
+            // Log the passed in text.
+            Debug.Log(logText);
+            DeveloperConsoleBehaviour.OutputMessage = $"Logged: {logText}";
+            // get ref, call the function to update the message
+            DeveloperConsoleBehaviour developerConsoleBehaviour = GameObject.FindObjectOfType<DeveloperConsoleBehaviour>();
+            developerConsoleBehaviour.UpdateOutputMessage();
+
+            return true;
+        }
+    }
+}
+
