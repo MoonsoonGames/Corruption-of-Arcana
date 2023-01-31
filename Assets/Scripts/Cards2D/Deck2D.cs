fdce1bd0def893caa3fa5ac80a2799a04981ed78
AJScott <as243879@falmouth.ac.uk>
2023-01-31 14:37:19 +0000

    feat: Weapons
    -Fixed weapon previews

--- Assets/Scripts/Cards2D/Deck2D.cs
+++ Assets/Scripts/Cards2D/Deck2D.cs
@@ -43,6 +43,7 @@ namespace Necropanda
         public int maxCards = 3;
         public int CurrentCardsLength()
         {
+            if (cards == null) return 0;
             return cards.Length;
         }
 
@@ -193,6 +194,7 @@ namespace Necropanda
         /// </summary>
         public void RemoveAllCards(bool discard)
         {
+            ResetArrays();
             foreach (CardDrag2D card in cards)
             {
                 DrawCard drawCard = card.GetComponent<DrawCard>();
@@ -225,7 +227,8 @@ namespace Necropanda
             }
 
             cards = new CardDrag2D[0];
-            timeline.SimulateSpellEffects();
+            if (timeline != null)
+                timeline.SimulateSpellEffects();
         }
 
         /// <summary>
