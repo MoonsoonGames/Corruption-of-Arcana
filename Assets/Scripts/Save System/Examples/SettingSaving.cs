6e8937eb22124ce0aa315999635e5c8971e99274
mr232432 <mr232432@falmouth.ac.uk>
2023-03-10 15:12:54 +0000

    fix/feat: fixed slider bug, implemented new checks
    Fixed the bug reported by vivii#8283 on the discord where the sliders were not showing any value on first launch. This fix brings a new system that checks to see if the game has been ran before.

--- Assets/Scripts/Save System/Examples/SettingSaving.cs
+++ Assets/Scripts/Save System/Examples/SettingSaving.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Diagnostics;
@@ -25,6 +26,39 @@ namespace Necropanda.SaveSystem
 
         [Header("Other")]
         public Stopwatch stopwatch;
+        [SerializeField] private string startId = string.Empty;
+
+        /// <summary>
+        /// Checks to see whether the game has been started before
+        /// </summary>
+        private void GenerateStartId()
+        {
+            if (CheckForFirstRun())
+            {
+                // LoadSliders();
+                return;
+            }
+
+            if (CheckForFirstRun() == false)
+            {
+                UnityEngine.Debug.Log("created slider start ID");
+                startId = Guid.NewGuid().ToString();
+                PlayerPrefs.SetString("StartID", startId);
+                PlayerPrefs.Save();
+
+                // Set the sliders for first usage
+                master.value = 1;
+                music.value = 1;
+                sfx.value = 1;
+                dialogue.value = 1;
+            }
+        }
+
+        private void Start()
+        {
+            GenerateStartId();
+
+        }
 
         public void SaveSliders()
         {
@@ -33,6 +67,7 @@ namespace Necropanda.SaveSystem
             PlayerPrefs.SetFloat("music", music.value);
             PlayerPrefs.SetFloat("sfx", sfx.value);
             PlayerPrefs.SetFloat("dialogue", dialogue.value);
+            SaveSettings();
         }
 
         public void LoadSliders()
@@ -46,10 +81,35 @@ namespace Necropanda.SaveSystem
 
         public void SaveSettings()
         {
+            stopwatch = new Stopwatch();
             stopwatch.Start();
             PlayerPrefs.Save();
             stopwatch.Stop();
             UnityEngine.Debug.Log($"Settings save completed in {stopwatch.Elapsed}");
+        }
+
+        /// <summary>
+        /// Checks to see whether the game has ran before based on if an ID string is found in the player prefs
+        /// </summary>
+        /// <returns>returns true if the game has ran, false if it hasn't</returns>
+        public bool CheckForFirstRun()
+        {
+            if (PlayerPrefs.HasKey("StartID"))
+            {
+                startId = PlayerPrefs.GetString("StartID");
+                return true;
+            }
+            else
+            {
+                // If there's no ID found, return false
+                return false;
+            }
+        }
+
+        [ContextMenu("Delete data")]
+        public void DeletePlayerPrefsData()
+        {
+            PlayerPrefs.DeleteAll();
         }
     }
 }
