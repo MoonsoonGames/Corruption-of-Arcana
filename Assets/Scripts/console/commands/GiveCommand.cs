2d4c8e7ac2c1b2bdbd92b444019946cbf2b48cbd
mr232432 <mr232432@falmouth.ac.uk>
2023-03-15 12:55:48 +0000

    feat: Item finding now works

--- Assets/Scripts/console/commands/GiveCommand.cs
+++ Assets/Scripts/console/commands/GiveCommand.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Necropanda.Utils.Console;
+using UnityEditor;
+using System;
 
 /// <summary>
 /// Authored & Written by @mattordev
@@ -14,10 +15,42 @@ namespace Necropanda.Utils.Console.Commands
     /// <summary>
     /// Allows the player to add items to thier inventory using the console
     /// </summary>
-    public class GiveCommand : ConsoleCommand
+    public class GiveCommand : ConsoleCommand, ISerializationCallbackReceiver
     {
+        // Unity doesn't know how to serialize dictionaries, so we make lists to show them in the inspector
         [Header("Giveable Objects:")]
-        public Dictionary<string, object> objects;
+        public List<string> _objectKeys = new List<string>();
+        public List<UnityEngine.Object> _objectValues = new List<UnityEngine.Object>();
+
+        public Dictionary<string, UnityEngine.Object> objects;
+
+        #region Dictionary Serialsation Work around
+        public void OnBeforeSerialize()
+        {
+            _objectKeys.Clear();
+            _objectValues.Clear();
+
+            foreach (var kvp in objects)
+            {
+                _objectKeys.Add(kvp.Key);
+                _objectValues.Add(kvp.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            objects = new Dictionary<string, UnityEngine.Object>();
+
+            for (int i = 0; i != Math.Min(_objectKeys.Count, _objectValues.Count); i++)
+                objects.Add(_objectKeys[i], _objectValues[i]);
+        }
+
+        void OnGUI()
+        {
+            foreach (var kvp in objects)
+                GUILayout.Label("Key: " + kvp.Key + " value: " + kvp.Value);
+        }
+        #endregion
 
         public override bool Process(string[] args)
         {
@@ -26,29 +59,27 @@ namespace Necropanda.Utils.Console.Commands
             // The second is the amount to be given
             int amountToGive = int.Parse(args[1]);
 
-            // Check that the argument is in the dictionary
-            if (IsValidObject(objToGive))
-            {
-                // Check to see what message we need to pass to the console, and what next action we need to take
-                if (CheckArgsAmount(args) == 1)
-                {
-                    DeveloperConsoleBehaviour.OutputMessage = $"Giving the player a {objToGive}";
 
-                }
-                else
-                {
-                    DeveloperConsoleBehaviour.OutputMessage = $"Giving the player [{amountToGive}] {objToGive}";
-                }
+
+            // Check that the argument is in the dictionary
+            if (!IsValidObject(objToGive))
+            {
+                // if its not we return an error to the console
+                DeveloperConsoleBehaviour.OutputMessage = $"Object: \"{objToGive}\" isn't valid!";
             }
 
-            // if it is, we output the correct message to the console
-
-            // if its not we return an error to the console
-
-
-
-
-
+            // Check to see what message we need to pass to the console, and what next action we need to take
+            if (CheckArgsAmount(args) == 1)
+            {
+                // if it is, we output the correct message to the console
+                DeveloperConsoleBehaviour.OutputMessage = $"Giving the player a {objToGive}";
+                // Do stuff to add the item
+            }
+            else
+            {
+                DeveloperConsoleBehaviour.OutputMessage = $"Giving the player [{amountToGive}] {objToGive}";
+                // Do stuff to add more than one item
+            }
             throw new System.NotImplementedException();
         }
 
@@ -71,25 +102,47 @@ namespace Necropanda.Utils.Console.Commands
         }
 
 
-        [ContextMenu("Find Items")]
+        [ContextMenu("Find Givable Items")]
         /// <summary>
         /// Finds any giveable items across the project and correctly adds them to the dictionary. 
         /// Ideally should be ran before every build
         /// </summary>
         private void FindGiveableObjects()
         {
+            //Make sure all resources are loaded
+            var weapons = Resources.LoadAll("Weapons", typeof(Weapon));
+            var cards = Resources.LoadAll("Spells", typeof(Spell));
+
+            // clear the dictionary
+            objects = new Dictionary<string, UnityEngine.Object>();
+
+            // Find the items
             Weapon[] weaponsList = Resources.FindObjectsOfTypeAll<Weapon>();
-            Card[] cardList = Resources.FindObjectsOfTypeAll<Card>();
+            Spell[] cardList = Resources.FindObjectsOfTypeAll<Spell>();
+
+            Debug.Log($"Weapons length: {weaponsList.Length}");
+            Debug.Log($"Cards length: {cardList.Length}");
 
             // add all weapons to the dictionary
             foreach (Weapon weapon in weaponsList)
             {
                 objects.Add(weapon.name, weapon);
+                Debug.Log(weapon.name);
             }
             // add all Cards to the dictionary
-            foreach (Card card in cardList)
+            foreach (Spell card in cardList)
             {
                 objects.Add(card.name, card);
+                Debug.Log(card.name);
+            }
+        }
+
+        [ContextMenu("Log Dictionary Items")]
+        private void LogGiveableItemsDictionary()
+        {
+            foreach (KeyValuePair<string, UnityEngine.Object> items in objects)
+            {
+                Debug.Log("You have " + items.Value + " " + items.Key);
             }
         }
 
